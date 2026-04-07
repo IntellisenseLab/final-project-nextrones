e524472170b9739a8ef7235a36c5c524de41edfe
hasinidd <dilmahasini@gmail.com>
2026-04-07 21:56:54 +0530

    Add nav_slam_launch.py and update setup.py/RViz for Nav2 screenshot

--- ros2_ws/src/robot_bringup/setup.py
+++ ros2_ws/src/robot_bringup/setup.py
@@ -13,26 +13,33 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         
-        # 1. Include all launch files so ros2 launch can find them
+        # 1. Include all launch files
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.py'))),
         
-        # 2. Include your Nav2 params and other configuration files
+        # 2. Include all config files
         (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.rviz'))),
+
+        # 3. Include URDF
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.urdf'))),
         
-        # 3. Include the test scripts folder added by your friend
+        # 4. Include scripts
         (os.path.join('share', package_name, 'scripts'), glob(os.path.join('scripts', '*'))),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'pyserial', 'numpy'],
     zip_safe=True,
     maintainer='san',
     maintainer_email='divyangiwas.23@uom.lk',
-    description='Robot bringup and configuration for Semantic Navigation project - Nextrones',
-    license='Apache-2.0',
+    description='Semantic Navigation Robot Bringup',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            # If you write a python node here later, add it like this:
-            # 'test_node = robot_bringup.test_node:main',
+            'kobuki_driver = robot_bringup.kobuki_driver:main',
+            'kinect_bridge = robot_bringup.kinect_bridge:main',
+            'yolo_detection_node = robot_bringup.yolo_detection_node:main',
+            'task_manager = robot_bringup.task_manager_node:main',
+            'cmd_vel_test = robot_bringup.cmd_vel_test:main',
         ],
     },
 )
